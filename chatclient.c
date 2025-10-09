82698fb6984a9d1a34823a0e1b74d352ba884f3c
Veentikon <radzividlo.vitalii@gmail.com>
2025-10-09 16:15:51 -0500

    Fixed all buffer issues, made sure to send only expected number of bytes to socket, user login status is shown to other users in red color

--- chatclient.c
+++ chatclient.c
@@ -18,16 +18,53 @@
 //TODO 3
 // Read username from argv, validate and send it in a message to the server.
 
+void *listen_thread(void *arg) {
+    int *server_socket = (int *) arg;
+    char server_message[1024];
+    char exit[5] = "exit";
+
+    while (1) {
+        //int res = poll(*server_socket)
+	int n_bytes = recv(*server_socket, &server_message, sizeof(server_message), 0); 	
+	   
+        if (n_bytes > 0 && server_message[n_bytes-1] == '\n') {
+            server_message[n_bytes-1] = '\0';
+        } else if (n_bytes == 0) { // peer closed the connection
+	    printf("Server closed the connection\n");
+	    return 0;
+	} else if (n_bytes == -1) {
+	    printf("Socket error occured, exiting...\n");
+	    return 0;
+	}
+	
+	printf("%s\n", server_message);
+
+        if (strcmp(server_message, exit) == 0) {
+            return 0;
+        }
+    }
+    return 0;
+}
+
 
 int main(int argc, char *argv[]) {
     // Valiate user input
-    if (argc < 2) {
-        printf("Error: expected 1 argument <username>");
+    if (argc != 2) {
+        printf("Error: expected 1 argument <username>, received %d\n", argc);
         return -1;
     }
-
-    char username[256] = "Eva";   
-
+ 
+    char *username = argv[1];
+    char user_input[1024];
+    char command[32];
+    pthread_t thread_id;   
+ 
+    // User input options. I could keem tham in an array instead.
+    char exit[5] = "exit"; // How to properly terminate child thread?
+    char options[8] = "options";
+    char write[6] = "write";
+    char users[6] = "users";
+    
     // Create a socket
     int netsocket;
     netsocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -43,16 +80,70 @@ int main(int argc, char *argv[]) {
         printf("There was an error making the connection to server \n\n");
     }
 
-    // receive data from server 
+    // Perform handshake with the server
     char server_response[256];
     send(netsocket, username, sizeof(username), 0);
     recv(netsocket, &server_response, sizeof(server_response), 0);
-    
-    
-    printf("server>: %s\n", server_response);
-    recv(netsocket, &server_response, sizeof(server_response), 0);
-    printf("thread>: %s\n", server_response);
+   
+    // If handshake failed, stop
+    if (strcmp(server_response, username) != 0) {
+	fprintf(stderr, "%s\n", server_response);
+	return -1;
+    }
 
+    printf("Welcome %s\n", username);
+
+    // Initialize listener thread to listen and print server responses/messages
+    int result = pthread_create(&thread_id, NULL, listen_thread, (void *) &netsocket);
+    if (result != 0) {
+        fprintf(stderr, "Error creating thread: %d\n", result);
+    	return 1;
+    }
+    
+    
+    // REPL
+    while (1) {
+	fgets(user_input, sizeof(user_input), stdin);
+	int length = strlen(user_input); // relevant bytes to be sent
+	
+	// find first space ===================
+	char *space = strchr(user_input, ' ');
+	if (space != NULL) {
+	    size_t len = space - user_input;
+	    strncpy(command, user_input, len);
+            command[len] = '\0';
+	    
+	} else {
+	    strcpy(command, user_input);
+	    command[strcspn(command, "\n")] = '\0';
+	}
+	// ====================================
+
+
+        // Remove trailing newline character
+        if (length > 0 && user_input[length-1] == '\n') {
+            user_input[length-1] = '\0';
+        }
+
+	// Test input command and send it to the server
+	if (strcmp(user_input, exit) == 0) {
+            printf("Bye %s\n", username);
+            send(netsocket, exit, sizeof(exit), 0);
+	    break; // Exit the loop
+        } else if (strcmp(command, options) == 0) {
+            printf("Options: options, write, users, exit\n");
+        } else if (strcmp(command, write) == 0) {
+            send(netsocket, user_input, sizeof(user_input), 0);
+        } else if (strcmp(command, users) == 0) {
+            send(netsocket, users, sizeof(users), 0);
+        } else {
+            printf("Unexpected input\n");
+        }
+    } 
+    
+    // Wait for thread to finish, clean up socket and exit
+    printf("waiting for child thread to finish ...\n");
+    pthread_join(thread_id, NULL);
     close(netsocket);
     return 0;
 }
--- chatserver.c
+++ chatserver.c
@@ -2,21 +2,16 @@
 
 #include <stdio.h> // printf, fgets
 #include <stdlib.h> // malloc, free, exit
+#include <errno.h> // error messages and stuff
 
 #include <sys/types.h>
 #include <sys/socket.h> // socket, bind, listen, accept, connect
 #include <netinet/in.h> // sockaddr_in
 
 #include <string.h> // strcpy, strcmp, strtok
-//#include <uinstd.h>
-
-
+#include <unistd.h>
 //#include <pthread.h> // threads
 
-
-// should I implement chat for only 2 people or for more?
-// if more, then I need a proper structure
-// a simple array could work if the expected number of users is not too large
 
 User users[MAX_USERS]; // Record of active users
 int user_count = 0;
@@ -30,7 +25,7 @@ int add_user(const char *username, int socket) {
         pthread_mutex_unlock(&user_mutex);
         return -1; 
     }
-    strncpy(users[user_count].username, username, USERNAME_LEN -1 );
+    strncpy(users[user_count].username, username, USERNAME_LEN -1);
     users[user_count].socket = socket;
     user_count ++;
     pthread_mutex_unlock(&user_mutex);
@@ -53,13 +48,40 @@ void remove_user(int socket) {
 void broadcast_message(const char *msg, int from_socket, char *from_user) {
     pthread_mutex_lock(&user_mutex);
     char buffer[1024] = "";
+    int n_bytes = 0;
     strcat(buffer, from_user);
+    n_bytes += strlen(from_user);
+    strcat(buffer, ": ");
     strcat(buffer, msg);
-    strcat(buffer, "\n");
+    n_bytes += strlen(msg); // ===================================================== Error?
+    n_bytes += 3; // add all the previous characters to length
     for (int i = 0; i < user_count; i ++) {
         if (users[i].socket != from_socket) {
-            send(users[i].socket, buffer, sizeof(buffer), 0);
+            send(users[i].socket, buffer, n_bytes, 0);
         }
+    }
+    pthread_mutex_unlock(&user_mutex);
+}
+
+
+// Send a notification to a user such as when other user joins or leaves the chat
+void notify_users(const char *msg, int from_socket, char *from_user) {
+    pthread_mutex_lock(&user_mutex);
+    char buffer[256] = "\033[1;31m";
+    int n_bytes = 10;
+    strcat(buffer, from_user);
+    n_bytes += strlen(from_user);
+    strcat(buffer, " ");
+    n_bytes += 1;
+    strcat(buffer, msg);
+    n_bytes += strlen(msg) + 7;
+    strcat(buffer, "\033[0m");
+    buffer[n_bytes] = '\0';
+    
+    for (int i = 0; i < user_count; i ++) {
+        if (users[i].socket != from_socket) {
+	    send(users[i].socket, buffer, n_bytes, 0);
+	}
     }
     pthread_mutex_unlock(&user_mutex);
 }
@@ -67,15 +89,21 @@ void broadcast_message(const char *msg, int from_socket, char *from_user) {
 // Send a list of active users to the requesting user
 void list_users(int client_socket) {
     pthread_mutex_lock(&user_mutex);
-    char buffer[1024] = "";
+    char buffer[256] = "online users: ";
+
+    // make sure to send only n_bytes of data, not the whole buffer   
+    int n_bytes = 14;
     for (int i = 0; i < user_count; i ++) {
         strcat(buffer, users[i].username);
-        strcat(buffer, "\n");
+	strcat(buffer, " | ");
+	n_bytes = n_bytes + sizeof(users[i].username) + 3;
     }
-    send(client_socket, buffer, sizeof(buffer), 0);
+    strcat(buffer, "\0"); // =============================
+    send(client_socket, buffer, n_bytes + 1, 0);
     pthread_mutex_unlock(&user_mutex);
 }
 
+// Test whether a user with specified username already exists
 int user_exists(char *username) {
     pthread_mutex_lock(&user_mutex);
     for (int i = 0; i < user_count; i ++) {
@@ -92,21 +120,82 @@ int user_exists(char *username) {
 // thread that communicates with the user
 // TODO: Finish implementation
 void *serve_thread(void *arg) {
-    char msg[256] = "Hello from the child thread";
-    int* client_socket = (int *) arg; 
-    printf("thread created, sending message on socket: %d\n", *client_socket);
-    send(*client_socket, msg, sizeof(msg), 0);
+    User *user = (User *)arg;
+    char client_message[256]; // full client message
+    char command[32]; // command received
+    char *message; // the message if user wants to send it
 
+    char users[6] = "users";
+    char write[6] = "write";
+    char exit[5] = "exit";
+    char joined[7] = "joined";
+    char left[5] = "left";
+
+    notify_users(joined, user->socket, user->username); // notify others that this user joined the chat
+
+    while(1) { // continuously read and serve client requests
+
+        int n_bytes = recv(user->socket, &client_message, sizeof(client_message), 0);
+        //printf("%s\n", client_message); // ==================================================
+	if (n_bytes > 0) { // socket live, message received
+            client_message[n_bytes] = '\0'; //ensure null termination
+        } else if (n_bytes == 0) { // peer closed connection
+	    remove_user(user->socket);
+	    free(user);
+	    return 0;
+	} else if (n_bytes == -1) {
+	    if (errno == EINTR) {
+		// Interrupted by signal, retry
+		continue;
+	    } else {
+		perror("recv");
+		notify_users(left, user->socket, user->username);
+		remove_user(user->socket);
+		free(user);
+		return 0;
+	    }
+	}
+
+	// extract the command
+	char *space = strchr(client_message, ' ');
+	if (space != NULL) {
+	    size_t len = space - client_message;
+	    strncpy(command, client_message, len);
+	    command[len] = '\0';
+
+	    // remove trailing newline
+	    message = space + 1; // message starts after space
+	    message[strcspn(message, "\n")] = '\0';
+	} else {
+	    strcpy(command, client_message);
+	    command[strcspn(command, "\n")] = '\0';
+	    message = NULL;
+	}
+
+	if (strcmp(command, exit) == 0) {
+	    remove_user(user->socket);   
+            send(user->socket, exit, sizeof(exit), 0);
+	    notify_users(left, user->socket, user->username);
+	    remove_user(user->socket);
+	    free(user);
+	    return 0;
+        } else if (strcmp(command, users) == 0) {
+	    list_users(user->socket);
+	} else if (strcmp(command, write) == 0) {
+	    broadcast_message(message, user->socket, user->username);
+	}
+    }
+    free(user);
     return NULL;
 }
 
 
 int main(int argc, char *argv[]) {
-    struct user * chatroom[10]; // logged in users
-    char server_message[256]; // server message buffer
+    //struct User *chatroom[10]; // logged in users
     char client_message[256]; // client message bufferi
     char delim[10] = " ";
     pthread_t thread_id;
+    int client_socket;
     
     // create server socket
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -123,39 +212,56 @@ int main(int argc, char *argv[]) {
     printf("socket bound and listening for incoming connections...\n");
     listen(server_socket, 4);
 
-    int client_socket;
-    client_socket = accept(server_socket, NULL, NULL);
-    
-    recv(client_socket, &client_message, sizeof(client_message), 0); // First message should contain username
+    while (1 == 1) { 
+	client_socket = accept(server_socket, NULL, NULL);    
+	recv(client_socket, &client_message, sizeof(client_message), 0); // First message should contain username
 
-    // TODO: set up hanshake proceure, recv HELLO <username>, send, Welcome <username>
-    // Need a way to parse arguments. This is a custom "handshake" to confirm successful user "login"
-    // Only create a new user if the username was not taken
-    // use strtok() to separate string into tokens, if first token is "HELLO", second token should be username
-    // restricitions? username cannot be \t or \n or \0 or " ". Should be between 1 and 10 characters long
-    // strtok() is fine in non-multithreaded env, otherwise use re-entrant version strtok_r()
-    
-    char *username = strtok(client_message, delim);
-    if (!user_exists(username)) {
-        int result;
-	char buff[256] = "Hello ";
-	strcat(buff, username);
-        
-	send(client_socket, buff, sizeof(buff), 0);
-        result = pthread_create(&thread_id, NULL, serve_thread, (void *) &client_socket);
-        if (result != 0) {
-            fprintf(stderr, "Error creating thread: %d\n", result);
-            return 1;
-        }
-    } else {
-        char msg[256] = "Error, this user already exists\n";
-        send(client_socket, msg, sizeof(msg), 0);
+	// use strtok() to separate string into tokens, if first token is "HELLO", second token should be username
+	// restricitions? username cannot be \t or \n or \0 or " ". Should be between 1 and 10 characters long
+	// strtok() is fine in non-multithreaded env, otherwise use re-entrant version strtok_r()
+	    
+	char *username = strtok(client_message, delim);
+	if (!user_exists(username)) {
+		
+	    int result;
+	    //char buff[256] = "Hello ";
+	    //strcat(buff, username);
+	    add_user(username, client_socket);
+
+	    // Create new user struct to pass to thread
+	    User *new_user = malloc(sizeof(User));
+	    strcpy(new_user->username, username);
+	    new_user->socket = client_socket;
+	    // =====================================
+	   
+	    result = pthread_create(&thread_id, NULL, serve_thread, (void *) new_user);
+	    send(client_socket, username, sizeof(username), 0);// send back username confirming successful login
+	    if (result != 0) {
+	        fprintf(stderr, "Error creating thread: %d\n", result);
+	        return 1;
+	    }
+	} else {
+	    char msg[256] = "Error, this user already exists\n";
+	    send(client_socket, msg, sizeof(msg), 0);
+	}
     }
-    
-    pthread_join(thread_id, NULL); // wait for the new thread to finish
-    printf("Hello from the main thread!\n");
+   
+    //pthread_join(thread_id, NULL); // wait for the new thread to finish
+    //printf("Hello from the main thread!\n");
 
     close(server_socket);
     return 0;
 }
+
+
+/*
+Instead of having a separate messaging loop, what if the user have to type "write" for each message to be
+broadcast
+
+which is simpler to implement? the second option
+which is easier otion to use? the first option
+*/
+
+
+
 
